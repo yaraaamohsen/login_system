77d56aa4b5cdfe4f6a57667014694d80292ae6e7
0yaramohsen00@gmail.com <0yaramohsen00@gmail.com>
2024-09-02 01:12:44 +0200

    second commit

--- js/index.js
+++ js/index.js
@@ -31,26 +31,32 @@ passInput.setAttribute('placeholder', 'Enter Your Password')
 passInput.classList.add('form-control', 'my-4');
 div.appendChild(passInput);
 
+var validPass = document.createElement('p');
+validPass.classList.add('alert-danger', 'alert', 'fs-5', 'd-none')
+div.appendChild(validPass);
+var validPassText = document.createTextNode('password must contain at least 8 charcters & small and large charcter');
+validPass.appendChild(validPassText);
+
 var success = document.createElement('p');
-success.classList.add('text-success', 'fs-4', 'd-none')
+success.classList.add('alert', 'alert-success', 'fs-4', 'd-none')
 div.appendChild(success);
 var successText = document.createTextNode('success');
 success.appendChild(successText);
 
 var emailFound = document.createElement('p');
-emailFound.classList.add('text-danger', 'fs-4', 'd-none')
+emailFound.classList.add('alert-danger', 'alert', 'fs-4', 'd-none')
 div.appendChild(emailFound);
 var emailFoundText = document.createTextNode('email is registed');
 emailFound.appendChild(emailFoundText);
 
 var danger = document.createElement('p');
-danger.classList.add('text-danger', 'fs-5', 'd-none')
+danger.classList.add('alert-danger', 'alert', 'fs-5', 'd-none')
 div.appendChild(danger);
 var dangerText = document.createTextNode('All inputs is required');
 danger.appendChild(dangerText);
 
 var invalid = document.createElement('p');
-invalid.classList.add('text-danger', 'fs-5', 'd-none')
+invalid.classList.add('alert-danger', 'alert', 'fs-5', 'd-none')
 div.appendChild(invalid);
 var invalidText = document.createTextNode('Email or password incorrect');
 invalid.appendChild(invalidText);
@@ -59,7 +65,7 @@ var buttonIn = document.createElement('button');
 div.appendChild(buttonIn);
 var textBI = document.createTextNode('Log In');
 buttonIn.appendChild(textBI);
-buttonIn.classList.add('btn', 'btn-outline-info', 'w-100', 'd-none');
+buttonIn.classList.add('btn', 'btn-outline-info', 'd-none', 'w-100');
 
 var buttonUp = document.createElement('button');
 div.appendChild(buttonUp);
@@ -121,13 +127,12 @@ else {
 }
 
 function emailRegistered() {
-    if(localStorage.getItem('User Information') == null){
+    if (localStorage.getItem('User Information') == null) {
         return false;
     }
-    else{
+    else {
         var y = JSON.parse(localStorage.getItem('User Information'));
         for (var i = 0; i < y.length; i++) {
-           
             if (emailInput.value == y[i].UserEmail) {
                 return true;
             }
@@ -135,18 +140,18 @@ function emailRegistered() {
     }
 }
 
-emailInput.addEventListener('click', function(){
-    if(!emailFound.classList.contains('d-none')){
+emailInput.addEventListener('click', function () {
+    if (!emailFound.classList.contains('d-none')) {
         emailFound.classList.add('d-none');
     }
 })
 
 buttonUp.addEventListener('click', function () {
-    if(emailRegistered() === true){
+    if (emailRegistered() === true) {
         emailFound.classList.remove('d-none');
         success.classList.add('d-none');
     }
-    else{
+    else {
         validation();
         if (danger.classList.contains('d-none')) {
             emailFound.classList.add('d-none');
@@ -167,17 +172,59 @@ function clearForm() {
     nameInput.value = null;
     emailInput.value = null;
     passInput.value = null;
+    nameInput.classList.remove('is-valid', 'is-invalid');
+    emailInput.classList.remove('is-valid', 'is-invalid');
+    passInput.classList.remove('is-valid', 'is-invalid');
 }
 
-function validation() {
+(function inputValidation(){
     var regex = {
-        nameInput: /^[a-z||A-Z]\w{2,}/,
+        nameInput: /^[a-z||A-Z]\w{3,20}/,
         emailInput: /^[\w\.]+@([\w-]+\.)+[\w-]{2,4}$/,
         passInput: /^[a-z||A-Z]\w{8,}/
     }
-    if (regex['nameInput'].test(nameInput.value) &&
-        regex['emailInput'].test(emailInput.value) &&
-        regex['passInput'].test(passInput.value)) {
+    
+    nameInput.addEventListener('input', function () {
+        if (regex['nameInput'].test(nameInput.value)) {
+          nameInput.classList.add('is-valid');
+          nameInput.classList.remove('is-invalid')
+        }
+        else{
+          nameInput.classList.remove('is-valid');
+          nameInput.classList.add('is-invalid')
+        }
+    }) 
+
+    emailInput.addEventListener('input', function () {
+        if (regex['emailInput'].test(emailInput.value)) {
+          emailInput.classList.add('is-valid');
+          emailInput.classList.remove('is-invalid')
+        }
+        else{
+          emailInput.classList.remove('is-valid');
+          emailInput.classList.add('is-invalid')
+        }
+    }) 
+
+    passInput.addEventListener('input', function () {
+        if (regex['passInput'].test(passInput.value)) {
+          passInput.classList.add('is-valid');
+          passInput.classList.remove('is-invalid');
+          validPass.classList.add('d-none')
+        }
+        else{
+          passInput.classList.remove('is-valid');
+          passInput.classList.add('is-invalid');
+          validPass.classList.remove('d-none')
+        }
+    })
+})()
+
+
+function validation() {
+    if (nameInput.classList.contains('is-valid') &&
+    emailInput.classList.contains('is-valid') &&
+    passInput.classList.contains('is-valid')) {
         success.classList.remove('d-none');
         danger.classList.add('d-none');
     }
@@ -194,6 +241,7 @@ function logInPage() {
     buttonIn.classList.remove('d-none');
     buttonUp.classList.add('d-none');
     success.classList.add('d-none');
+   clearForm()
 }
 
 aI.addEventListener('click', function () {
@@ -206,23 +254,29 @@ aU.addEventListener('click', function () {
     pU.classList.add('d-none');
     buttonIn.classList.add('d-none');
     buttonUp.classList.remove('d-none');
-    success.classList.add('d-none')
+    success.classList.add('d-none');
+   clearForm()
 })
 
 buttonIn.addEventListener('click', function () {
-    var x = JSON.parse(localStorage.getItem('User Information'));
-    for (var i = 0; i < x.length ; i++){
-        setTimeout(() => {
-            invalid.classList.remove('d-none');
-        }, 1000);
-        if (emailInput.value == x[i].UserEmail &&
-            passInput.value == x[i].UserPass) {
+    if (localStorage.getItem('User Information') == null) {
+        invalid.classList.remove('d-none')
+    } else {
+        invalid.classList.add('d-none')
+        var x = JSON.parse(localStorage.getItem('User Information'));
+        for (var i = 0; i < x.length; i++) {
+            setTimeout(() => {
+                invalid.classList.remove('d-none');
+            }, 1000);
+            if (emailInput.value == x[i].UserEmail &&
+                passInput.value == x[i].UserPass) {
                 console.log(x[i].UserEmail);
                 invalid.classList.add('d-none');
-            setTimeout(() => {
-                window.location.href = 'home.html'; 
-            }, 500);
-            localStorage.setItem('userName', x[i].userName);
+                setTimeout(() => {
+                    window.location.href = 'home.html';
+                }, 500);
+                localStorage.setItem('userName', x[i].userName);
+            }
         }
     }
 })
